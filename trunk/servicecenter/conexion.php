7da64238b5acec4636bb7c5485784106043efe39
vincenzo84ve@gmail.com <vincenzo84ve@gmail.com@f4ad7cbc-8b8b-11de-8988-25e8a898b690>
2009-09-10 01:50:01 +0000

    cambios al modulo de requerimientos

--- trunk/servicecenter/conexion.php
+++ trunk/servicecenter/conexion.php
@@ -3,7 +3,7 @@ class Conexion{
     var $conexion_bd;
 
     function conectar(){// Conectar a la base de datos
-        $this->conexion_bd = pg_connect("host=localhost port=5432 dbname=scdat user=postgres password=12345678") or die('No pudo conectarse: ' . pg_last_error());
+        $this->conexion_bd = pg_connect("host=190.109.100.36 port=5432 dbname=scdat user=postgres password=invepal1nv3p4l") or die('No pudo conectarse: ' . pg_last_error());
         return ($this->conexion_bd);
     }
 
